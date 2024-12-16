c590dabb03659e3d13bba17ba0948ed775279287
akazemih <akazemih@gmail.com>
2024-12-16 17:21:27 +0330

    Update header-footer-inject.js

--- assets/js/header-footer-inject.js
+++ assets/js/header-footer-inject.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Select header and footer containers
     let headerContainer = document.querySelector(".header");
-    let footerContainer = document.querySelector(".footer");
 
     // Function to fetch and inject content into a container
-    async function injectContent(url, container) {
+    async function injectContent(url) {
         try {
             const response = await fetch(url);
             if (!response.ok) {
