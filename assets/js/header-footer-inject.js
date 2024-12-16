3afa2995b3c317139ae149493bfc57cd6110a046
akazemih <akazemih@gmail.com>
2024-12-16 17:44:51 +0330

    Update header-footer-inject.js

--- assets/js/header-footer-inject.js
+++ assets/js/header-footer-inject.js
@@ -8,13 +8,13 @@ document.addEventListener("DOMContentLoaded", function() {
         try {
             const response = await fetch(url);
             if (!response.ok) {
-                throw new Error(Failed to load content from ${url});
+                throw new Error(`Failed to load content from ${url}`);
             }
             const html = await response.text();
             container.innerHTML = html;
         } catch (error) {
             console.error(error);
-            container.innerHTML = <p>خطا در بارگذاری محتوا!</p>;
+            container.innerHTML = `<p>خطا در بارگذاری محتوا!</p>`;
         }
     }
 
