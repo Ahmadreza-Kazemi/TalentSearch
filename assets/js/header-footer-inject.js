2e87285e906768d0d7013fa2416c2d35b0a55381
akazemih <akazemih@gmail.com>
2024-12-19 09:29:28 +0330

    Update header-footer-inject.js

--- assets/js/header-footer-inject.js
+++ assets/js/header-footer-inject.js
@@ -86,3 +86,12 @@ document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("click", r);
     document.addEventListener("keydown", l);
 });
+    // Closes the menu if a click is detected outside of the menu and toggle button
+    function r(n) {
+        if (!e.contains(n.target) && !t.contains(n.target)) s();
+    }
+
+    // Closes the menu if the Escape key is pressed
+    function l(t) {
+        if (t.key === "Escape") s();
+    }
